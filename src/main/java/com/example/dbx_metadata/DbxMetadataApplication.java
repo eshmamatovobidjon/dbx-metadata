113493a6bc1c13855056b9ce77a09969659abef3
Obidjon_Eshmamatov <Obidjon.Eshmamatov@wolterskluwer.com>
2025-12-07 12:03:57 +0500

    initial commit

---
+++ src/main/java/com/example/dbx_metadata/DbxMetadataApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbx_metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbxMetadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbxMetadataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dbx_metadata/DbxMetadataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbx_metadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbxMetadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
